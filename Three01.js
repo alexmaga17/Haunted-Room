d58d92a902260fa68cce15169dd417b2b61a4097
alexmaga17 <alexmagalhaes17@gmail.com>
2022-01-15 16:41:40 +0000

    funcs

--- Three01.js
+++ Three01.js
@@ -14,7 +14,7 @@ import { GLTFLoader } from './libs/GLTFLoader.js';
 
     // PAREDES //
     let walls = new THREE.TextureLoader().load("textures/paredes4.jpg");
-    walls.repeat.set(1.5,1.5);
+    walls.repeat.set(1.5,1,5);
     walls.wrapS = walls.wrapT = THREE.MirroredRepeatWrapping;
 
     // TECLAS //
@@ -26,13 +26,147 @@ import { GLTFLoader } from './libs/GLTFLoader.js';
 
     window.onload = function init() {
         alert("BEM VINDO!\nPARA MOVER O TEU PERSONAGEM UTILIZA AS TECLAS WASD!");
-    } 
+        newScene();
+        
+    }
+
+            // CHÃO //
+            function chao(){
+                let geometryFloor = new THREE.PlaneGeometry(50, 80);
+                let materialFloor = new THREE.MeshBasicMaterial({ map: ground });
+                let floor = new THREE.Mesh(geometryFloor, materialFloor);
+                floor.rotation.x = - Math.PI / 2;
+                floor.name = "floor";
+                scene.add(floor);
+                renderer.render(scene, camera);
+            }
+            
+
+            // TETO //
+            function teto(){
+                let geometryRoof = new THREE.PlaneGeometry(50, 80);
+                let materialRoof = new THREE.MeshBasicMaterial({ map: ceiling });
+                let roof = new THREE.Mesh(geometryRoof, materialRoof);
+                roof.rotation.x = Math.PI / 2;
+                roof.name = "roof";
+                scene.add(roof);
+                roof.position.set(0,14,3);
+            }
+
+            // PAREDES //
+            function paredes(){
+                // PAREDE 1 //
+                let geometryWall1 = new THREE.PlaneGeometry(80, 14);
+                let materialWall1 = new THREE.MeshBasicMaterial({ map: walls });
+                let wall1 = new THREE.Mesh(geometryWall1, materialWall1);
+                wall1.rotation.y = Math.PI / 2;
+                wall1.name = "wall1";
+                scene.add(wall1);
+                wall1.position.set(-25,7,0.5);
+
+                // PAREDE 2 //
+                let geometryWall2 = new THREE.PlaneGeometry(80, 14);
+                let materialWall2 = new THREE.MeshBasicMaterial({ map: walls });
+                let wall2 = new THREE.Mesh(geometryWall2, materialWall2);
+                wall2.rotation.y = - Math.PI / 2;
+                wall2.name = "wall2";
+                scene.add(wall2);
+                wall2.position.set(25,7,0.5);
+
+                // PAREDE 3 //
+                let geometryWall3 = new THREE.PlaneGeometry(14, 50);
+                let materialWall3 = new THREE.MeshBasicMaterial({ map: walls });
+                let wall3 = new THREE.Mesh(geometryWall3, materialWall3);
+                wall3.rotation.z =  -Math.PI / 2;
+                wall3.name = "wall3";
+                scene.add(wall3);
+                wall3.position.set(0,7,-37);
+
+                // PAREDE 4 //
+                let geometryWall4 = new THREE.PlaneGeometry(14, 50);
+                let materialWall4 = new THREE.MeshBasicMaterial({ map: walls });
+                let wall4 = new THREE.Mesh(geometryWall4, materialWall4);
+                wall4.rotation.z =  -Math.PI / 2;
+                wall4.name = "wall4";
+                scene.add(wall4);
+                wall4.position.set(0,5,40);
+            }
+
+            
+
+            // DOOR //
+            
+            let geometryDoor = new THREE.PlaneGeometry();
+            let materialDoor = new THREE.MeshBasicMaterial({});
+            let door = new THREE.Mesh(geometryDoor, materialDoor);
+
+            // LAMPADA //
+            function lampada(){
+                const loader = new GLTFLoader();
+                loader.load('./textures/lamp_ceiling_-_fnaf/scene.gltf', function(gltf){
+                    const lamp = gltf.scene;
+                    lamp.position.set(0,11.5,-6);
+                    lamp.scale.set(0.008,0.008,0.008)
+                    scene.add(lamp);
+                }, function(xhr){
+                    console.log((xhr.loaded/xhr.total * 100) + "%loaded")
+                }, function(error){
+                    console.log("error")
+                });
+            }
+
+            // MESA //
+            function mesa(){
+                const loader2 = new GLTFLoader();
+                loader2.load('./textures/mesa/scene.gltf', function(gltf){
+                    const mesa = gltf.scene;
+                    mesa.position.set(-23,1.5,-6);
+                    mesa.scale.set(3.5,3.5,3.5);
+                    // mesa.rotation.y =  - Math.PI / 2;
+                    scene.add(mesa);
+                }, function(xhr){
+                    console.log((xhr.loaded/xhr.total * 100) + "%loaded")
+                }, function(error){
+                    console.log("error")
+                });
+            }
+
+            // CADEIRA //
+            function cadeira(){
+                const loader3 = new GLTFLoader();
+                loader3.load('./textures/cadeira/scene.gltf', function(gltf){
+                    const cadeira = gltf.scene;
+                    cadeira.position.set(-16,1.5,-6);
+                    cadeira.scale.set(0.013,0.013,0.013);
+                    cadeira.rotation.y =  - Math.PI / 2;
+                    scene.add(cadeira);
+                }, function(xhr){
+                    console.log((xhr.loaded/xhr.total * 100) + "%loaded")
+                }, function(error){
+                    console.log("error")
+                });
+            }
+
+            // JANELA //
+            function janela(){
+                const loader4 = new GLTFLoader();
+                loader4.load('./textures/janela/scene.gltf', function(gltf){
+                    const janela = gltf.scene;
+                    janela.position.set(0,7,-36.5);
+                    janela.scale.set(5,5,5);
+                    scene.add(janela);
+                }, function(xhr){
+                    console.log((xhr.loaded/xhr.total * 100) + "%loaded")
+                }, function(error){
+                    console.log("error")
+                });
+            }
     
     function newScene(){
         scene = new THREE.Scene();
 
             const aspect = window.innerWidth / window.innerHeight;
-            camera = new THREE.PerspectiveCamera(90, aspect, 0.001, 250);
+            camera = new THREE.PerspectiveCamera(90, aspect, 1.0, 1000);
             camera.position.x = camera.position.y = 2; // place the camera using world coordinates
             camera.position.set(0,5,10);
             camera.position.z = 10;
@@ -51,114 +185,26 @@ import { GLTFLoader } from './libs/GLTFLoader.js';
             // light2.position.set(0,-10,-6);
             // light2.target.position.set(0,-150,-6);
             // scene.add(light2);
-
-            // CHÃO //
-            let geometryFloor = new THREE.PlaneGeometry(50, 80);
-            let materialFloor = new THREE.MeshBasicMaterial({ map: ground });
-            let floor = new THREE.Mesh(geometryFloor, materialFloor);
-            floor.rotation.x = - Math.PI / 2;
-            floor.name = "floor";
-            scene.add(floor);
-            
-
-            // TETO //
-            let geometryRoof = new THREE.PlaneGeometry(50, 80);
-            let materialRoof = new THREE.MeshBasicMaterial({ map: ceiling });
-            let roof = new THREE.Mesh(geometryRoof, materialRoof);
-            roof.rotation.x = Math.PI / 2;
-            roof.name = "roof";
-            scene.add(roof);
-            roof.position.set(0,14,3);
-
-            // PAREDE 1 //
-            let geometryWall1 = new THREE.PlaneGeometry(80, 14);
-            let materialWall1 = new THREE.MeshBasicMaterial({ map: walls });
-            let wall1 = new THREE.Mesh(geometryWall1, materialWall1);
-            wall1.rotation.y = Math.PI / 2;
-            wall1.name = "wall1";
-            scene.add(wall1);
-            wall1.position.set(-25,7,0.5);
-
-            // PAREDE 2 //
-            let geometryWall2 = new THREE.PlaneGeometry(80, 14);
-            let materialWall2 = new THREE.MeshBasicMaterial({ map: walls });
-            let wall2 = new THREE.Mesh(geometryWall2, materialWall2);
-            wall2.rotation.y = - Math.PI / 2;
-            wall2.name = "wall2";
-            scene.add(wall2);
-            wall2.position.set(25,7,0.5);
-
-            // PAREDE 3 //
-            let geometryWall3 = new THREE.PlaneGeometry(14, 50);
-            let materialWall3 = new THREE.MeshBasicMaterial({ map: walls });
-            let wall3 = new THREE.Mesh(geometryWall3, materialWall3);
-            wall3.rotation.z =  -Math.PI / 2;
-            wall3.name = "wall3";
-            scene.add(wall3);
-            wall3.position.set(0,7,-37);
-
-            // PAREDE 4 //
-            let geometryWall4 = new THREE.PlaneGeometry(14, 50);
-            let materialWall4 = new THREE.MeshBasicMaterial({ map: walls });
-            let wall4 = new THREE.Mesh(geometryWall4, materialWall4);
-            wall4.rotation.z =  -Math.PI / 2;
-            wall4.name = "wall4";
-            scene.add(wall4);
-            wall4.position.set(0,5,40);
-
-            
-
-            // DOOR //
-            
-            let geometryDoor = new THREE.PlaneGeometry();
-            let materialDoor = new THREE.MeshBasicMaterial({});
-            let door = new THREE.Mesh(geometryDoor, materialDoor);
-
-            // LAMPADA //
-            const loader = new GLTFLoader();
-            loader.load('./textures/lamp_ceiling_-_fnaf/scene.gltf', function(gltf){
-                const lamp = gltf.scene;
-                lamp.position.set(0,11.5,-6);
-                lamp.scale.set(0.008,0.008,0.008)
-                scene.add(lamp);
-            }, function(xhr){
-                console.log((xhr.loaded/xhr.total * 100) + "%loaded")
-            }, function(error){
-                console.log("error")
-            });
-
-            // MESA //
-            const loader2 = new GLTFLoader();
-            loader2.load('./textures/mesa/scene.gltf', function(gltf){
-                const mesa = gltf.scene;
-                mesa.position.set(-23,1.5,-6);
-                mesa.scale.set(3.5,3.5,3.5);
-                // mesa.rotation.y =  - Math.PI / 2;
-                scene.add(mesa);
-            }, function(xhr){
-                console.log((xhr.loaded/xhr.total * 100) + "%loaded")
-            }, function(error){
-                console.log("error")
-            });
-
-            // CADEIRA //
-            const loader3 = new GLTFLoader();
-            loader3.load('./textures/cadeira/scene.gltf', function(gltf){
-                const cadeira = gltf.scene;
-                cadeira.position.set(-16,1.5,-6);
-                cadeira.scale.set(0.013,0.013,0.013);
-                cadeira.rotation.y =  - Math.PI / 2;
-                scene.add(cadeira);
-            }, function(xhr){
-                console.log((xhr.loaded/xhr.total * 100) + "%loaded")
-            }, function(error){
-                console.log("error")
-            });
-
+            // let geometry1 = new THREE.BoxGeometry(30, 51.8, 30);
+            // let material1 = new THREE.MeshBasicMaterial({ color: 0x008800, transparent: true, opacity: 0.0000001, name: "CameraBox" });
+            // cameraBox = new THREE.Mesh(geometry1, material1);
+            // cameraBox.rotation.y = Math.PI;
+            // cameraBox.position.set(0, 0, 100);
+            // scene.add(cameraBox);
+        
+            // cameraBox.geometry.computeBoundingBox();
+            chao();
+            teto();
+            paredes();
+            lampada();
+            mesa();
+            cadeira();
+            janela();
 
             // ANIMAR //
             renderer.setAnimationLoop(render);
             document.onkeydown = handleKeyDown;
+            
 
             // KEYS //
             function handleKeyDown(e){
@@ -176,9 +222,6 @@ import { GLTFLoader } from './libs/GLTFLoader.js';
                 if(key == "d" || key == "D"){
                     moveRight = true;
                 }
-
-
-                
                 if(key == "ArrowUp"){
                     camera.rotation.x += 0.06;
                     console.log(camera.rotation.x)
@@ -242,6 +285,6 @@ import { GLTFLoader } from './libs/GLTFLoader.js';
         //requestAnimationFrame(render);
     }
 
-    newScene();
+   
 
    
